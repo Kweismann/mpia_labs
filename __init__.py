dbd03135c501fdf3537918c1ef0c4757026d374a
Нечаев Виталий Викторович <v.nechaev.2015@stud.nstu.ru>
2016-10-11 10:24:24 +0300

    update  funcs

--- __init__.py
+++ __init__.py
@@ -1,8 +1,6 @@
-from mpiaa.timer import time_me
-from mpiaa.util import *
-from mpiaa.funcs import *
+from funcs import *
 
 
 if __name__ == "__main__":
-    file = "../record_gen/records_1e3.txt"
-    print(algorithm(file))
+    file = "../record_gen/records_1e4.txt"
+    print(algorithm(Input(file)))
--- funcs.py
+++ funcs.py
@@ -1,4 +1,3 @@
-import time
 import os
 
 
@@ -6,9 +5,9 @@ class People:
     pass
 
 
-def algorithm(file):
+def Input(file):
     if os.stat(file).st_size == 0:
-        return "Exception: file empty"
+        return None
     else:
         s = open(file)
         peoples = []
@@ -22,6 +21,13 @@ def algorithm(file):
             peoples[i].month = temp_people[3]
             peoples[i].year = temp_people[4]
             i += 1
+        return peoples
+
+
+def algorithm(peoples):
+    if peoples is None:
+        return "Exception: file empty"
+    else:
         max_day = [0, 0]
         for x in peoples:
             t_day = x.day
@@ -33,38 +39,3 @@ def algorithm(file):
                 max_day[0] = t_count
                 max_day[1] = t_day
         return max_day
-
-
-# def algorithm(uns, s):
-#     i = 0
-#     peoples = []
-#     for x in s:
-#         peoples.append(People())
-#         temp_people = x.split(' ')
-#         peoples[i].f_name = temp_people[0]
-#         peoples[i].l_name = temp_people[1]
-#         peoples[i].day = temp_people[2]
-#         peoples[i].month = temp_people[3]
-#         peoples[i].year = temp_people[4]
-#         i += 1
-#     max_day = [0, 0]
-#     for x in peoples:
-#         t_day = x.day
-#         t_count = 0
-#         for y in peoples:
-#             if t_day == y.day:
-#                 t_count += 1
-#         if max_day[0] <= t_count:
-#             max_day[0] = t_count
-#             max_day[1] = t_day
-#     return max_day
-
-
-def getTime(func, generate, numb, n):
-    func_time = 0
-    for x in range(n):
-        str = generate(10 ** numb)
-        t1 = time.time()
-        func(str)
-        func_time += time.time() - t1
-    return func_time / n
